079647dd77f64cb198eb9ec2c80c6de3e80fb5ab
patrick9958 <patrick.neggie@gmail.com>
2020-07-02 15:04:00 -0400

    hide and show function now works on checkbox and +/- buttons

--- FontChooser.js
+++ FontChooser.js
@@ -1,21 +1,30 @@
 class FontChooser extends React.Component {
 
     constructor(props) {
-	super(props);
+		super(props);
+		this.state = { is_hidden: true };
     }
-    
+
+    click_handle() {
+    	console.log('click_handle called');
+    	if (this.state.is_hidden) {
+			this.setState({is_hidden: false});
+		}
+    	else {
+			this.setState({is_hidden: true});
+		}
+	}
 
     render() {
-
-	return(
-	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
-	       </div>
-	);
+		return(
+			   <div>
+			   <input type="checkbox" id="boldCheckbox" hidden={this.state.is_hidden}/>
+			   <button id="decreaseButton" hidden={this.state.is_hidden}>-</button>
+			   <span id="fontSizeSpan" hidden={this.state.is_hidden}>{this.props.size}</span>
+			   <button id="increaseButton" hidden={this.state.is_hidden}>+</button>
+			   <span id="textSpan" onClick={this.click_handle.bind(this)}>{this.props.text}</span>
+			   </div>
+		);
     }
 }
 
